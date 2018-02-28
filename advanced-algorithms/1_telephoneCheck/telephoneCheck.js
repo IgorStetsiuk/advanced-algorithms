081ef3a56cccfe1f941017e7efe3e2f3cdd440c1
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-02-28 11:37:02 +0200

    first algorithm done

--- advanced-algorithms/1_telephoneCheck/telephoneCheck.js
+++ advanced-algorithms/1_telephoneCheck/telephoneCheck.js
@@ -1,19 +1,6 @@
-  export function telephoneCheck(str) {
-	// console.log(str.match(/^[1\s(]?\d{3}[-\s)]?\d{3}[-\s?]\d{4{10,}$/g));
-	// return !!str.match(/^([1\s(]?\d{3}[-\s)]?\d{3}[-\s?]\d{4})$/g);
-     // 555-555-5555
-     // (555)555-5555
-     // (555) 555-5555
-     // 555 555 5555
-     // 5555555555
-     // 1 555 555 5555
-	 console.log(str.match(/[1\s(]?\d{3}[)\s]?\d{3}[\s-]?\d{4}/g));
-     return !!str.match(/[1\s]?\(?\d{3}[)\s-]?\s?\d{3}[\s-]?\d{4}/g);
- }
+export  function telephoneCheck(str) {
+	return !!str.match(/^(\(\d{3}\)\s?|(1?\s?\d{3}-?\s?|1\s?\(\d{3}\)\s?))\d{3}[\s-]?\d{4}$/g);
+	
+}
 
-console.log(telephoneCheck("555-555-5555"));
-console.log(telephoneCheck("1 555-555-5555"));
-console.log(telephoneCheck("(555)555-5555"));
-console.log(telephoneCheck("5555555555"));
-console.log(telephoneCheck("1(555)555-5555"));
-console.log(telephoneCheck("1 (555) 555-5555"));
+
