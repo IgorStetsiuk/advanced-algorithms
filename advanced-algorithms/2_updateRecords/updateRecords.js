90b6e3032def9d42377d22caf62e3b1f235f209a
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-02-28 13:36:05 +0200

    move collection to saparate file

--- advanced-algorithms/2_updateRecords/updateRecords.js
+++ advanced-algorithms/2_updateRecords/updateRecords.js
@@ -1,31 +1,5 @@
-let collection = {
-	"2548": {
-		"album": "Slippery When Wet",
-		"artist": "Bon Jovi",
-		"tracks": [
-			"Let It Rock",
-			"You Give Love a Bad Name"
-		]
-	},
-	"2468": {
-		"album": "1999",
-		"artist": "Prince",
-		"tracks": [
-			"1999",
-			"Little Red Corvette"
-		]
-	},
-	"1245": {
-		"artist": "Robert Palmer",
-		"tracks": []
-	},
-	"5439": {
-		"album": "ABBA Gold"
-	}
-};
+import collection from './collection';
 
-
-// Only change code below this line
 export function updateRecords(id, prop, value) {
 	const record = collection[id];
 	
