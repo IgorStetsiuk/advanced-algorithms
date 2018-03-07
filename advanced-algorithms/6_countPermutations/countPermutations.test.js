9c6034bec11fe4a2af04ae668fd6f0818e10d53a
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-03-08 00:16:46 +0200

    change pairwuse algorithm

--- advanced-algorithms/6_countPermutations/countPermutations.test.js
+++ advanced-algorithms/6_countPermutations/countPermutations.test.js
@@ -1,49 +1,49 @@
 import {countPermutations} from './countPermutations';
 
 describe('countPermutations', () => {
-	
+
 	test('TEST_1 countPermutations("aab") should return a number.\n', () => {
 		const result = typeof countPermutations("aab");
 		expect(result).toEqual('number');
 	});
-	
+
 	test('TEST_2 countPermutations("aab") should return 2.\n', () => {
 		expect(countPermutations("aab")).toBe(2);
 	});
-	
+
 	test('TEST_3 countPermutations("aaa") should return 0.\n', () => {
 		expect(countPermutations("aaa")).toBe(0);
 	});
-	
+
 	test('TEST_4 countPermutations("aabb") should return 8.\n', () => {
 		expect(countPermutations("aabb")).toBe(8);
 	});
-	
+
 	test('TEST_5 countPermutations("abcdefa") should return 3600.', () => {
 		expect(countPermutations("abcdefa") ).toBe(3600);
 	});
-	
+
 	test('TEST_6 countPermutations("abfdefa") should return 2640.\n', () => {
 		expect(countPermutations("abfdefa") ).toBe(2640);
 	});
-	
+
 	test('TEST_7 countPermutations("zzzzzzzz") should return 0.\n', () => {
 		expect(countPermutations("zzzzzzzz")).toBe(0);
 	});
-	
+
 	test('TEST_8 countPermutations("a") should return 1.\n', () => {
 		expect(countPermutations("a")).toBe(1);
-		
+
 	});
-	
+
 	test('TEST_9 countPermutations("aaab") should return 0.\n', () => {
 		expect(countPermutations("aaab")).toBe(0);
 	});
-	
+
 	test('TEST_10 countPermutations("aaabb") should return 12.\n', () => {
 		expect(countPermutations("aaabb")).toBe(12);
 	});
-	
-	
-	
+
+
+
 });
--- advanced-algorithms/9_pairwise/pairwise.js
+++ advanced-algorithms/9_pairwise/pairwise.js
@@ -1,43 +1,21 @@
- export function pairwise(arr, arg) {
-	const array = [...arr];
-	let usedElements = [];
-	let forbiddenIndexes = [];
-	let sumsOfIndexes = arr.reduce((prev, next, currIndex, arr) => {
-		let arrayCopy = array.slice();
-		let slised =arrayCopy.splice(currIndex,1);
-
-		// let partsOfPair = arrayCopy.filter(el => next + el === arg);
-
-		let partsOfPairArr = slised.reduce((accum, el) => {
-			if (next + el === arg ){
-				accum.push({elem: el, index: array.indexOf(el)});
-				return [...accum]
+export function pairwise(arr, arg) {
+	
+	let result = 0;
+	let copyArray = [...arr];
+	let usedIndexes = [];
+	
+	
+	arr.forEach((el, index) => {
+		copyArray.forEach((copyElem, copyElIndex) => {
+			if (index !== copyElIndex) {
+				
+				if (el + copyElem === arg && usedIndexes.indexOf(index) === -1 && usedIndexes.indexOf(copyElIndex) === -1) {
+					result = result + index + copyElIndex;
+					usedIndexes.push(index, copyElIndex);
+				}
 			}
-			return [...accum]
-		}, []);
-
-		debugger
-
-		const pairsSumOfIndexes = partsOfPairArr.reduce((rez, {elem, index}) => {
-			if (!usedElements.includes(elem) && !forbiddenIndexes.includes(index)) {
-				usedElements.push(elem);
-				forbiddenIndexes.push(index);
-				let indexSum = currIndex + index;
-				return [...rez, indexSum];
-			}
-			return [...rez];
-		},[]);
-		debugger
-		next = [...prev, ...pairsSumOfIndexes];
-		usedElements = []
-		console.log(next);
-		return next;
-	}, []);
-
-	return Math.max(...sumsOfIndexes) + Math.min(...sumsOfIndexes);
+		})
+	});
+	
+	return result;
 }
-
-console.log(pairwise([1, 4, 2, 3, 0, 5], 7));
-console.log(pairwise([1, 3, 2, 4], 4));
-console.log(pairwise([0, 0, 0, 0, 1, 1], 1));
-console.log(pairwise([1, 4, 2, 3, 0, 5], 7));
