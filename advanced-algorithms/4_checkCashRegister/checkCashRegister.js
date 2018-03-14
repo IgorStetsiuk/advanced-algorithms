0c6faff71736921b3d58f6403fc6fed34cb3979e
Igor Stetsiuk <igorstetsuk@gmail.com>
2018-03-14 16:39:59 +0200

    Added the last algorithm

--- advanced-algorithms/4_checkCashRegister/checkCashRegister.js
+++ advanced-algorithms/4_checkCashRegister/checkCashRegister.js
@@ -35,14 +35,14 @@ export function checkCashRegister(price, cash, cid) {
 	}
 	
 	if (isTransactionalAlive && result !=='Insufficient Funds' && result !=='Closed') {
-		const register = cid.reduce(function (acc, curr) {
+		const register = cid.reduce( (acc, curr) => {
 			acc.total += curr[1];
 			acc[curr[0]] = curr[1];
 			return acc;
 		}, {total: 0});
 		
 		
-		result = currencyMetaData.reduce(function(acc, curr) {
+		result = currencyMetaData.reduce((acc, curr) =>{
 			let value = 0;
 			
 			while (register[curr.name] > 0 && rest >= curr.value) {
--- advanced-algorithms/6_countPermutations/countPermutations.js
+++ advanced-algorithms/6_countPermutations/countPermutations.js
@@ -1,5 +1,34 @@
 export function countPermutations(str) {
-	return str;
+	
+	
+	let combinations = [];
+	let arr = str.split('');
+	
+	let swapCharactersByIndex = function (arr, start, i) {
+		let temp = arr[start];
+		arr[start] = arr[i];
+		arr[i] = temp;
+		return arr;
+	};
+	
+	let getPermutations =  (array, start, stop, perms) => {
+		let i = null;
+		let permutation = [];
+		
+		if (start === stop) {
+			for (i = 0; i < array.length; i++) {
+				permutation.push(array[i]);
+			}
+			perms.push(permutation.join(''));
+		} else {
+			for (i = start; i < stop; i++) {
+				array = swapCharactersByIndex(array, start, i);
+				getPermutations(array, start + 1, stop, perms);
+				array = swapCharactersByIndex(array, start, i);
+			}
+		}
+	};
+	getPermutations(arr, 0, arr.length, combinations);
+	
+	return combinations.filter(perm => !perm.match(/(.)\1+/g)).length
 }
-
-countPermutations('aab');
